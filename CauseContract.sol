b7ef7fda6f8e2ff0504ad4ee73c3185c5b066685
finnformica <finnformica@gmail.com>
2023-04-20 15:09:21 +0100

    store incoming and outgoing transactions

--- CauseContract.sol
+++ CauseContract.sol
@@ -10,27 +10,52 @@ contract CauseContract {
     // human-readable contract id
     string id;
 
+    // transaction struct
+    struct Transaction {
+        address sender;
+        uint amount;
+    }
+
+    // incoming donations
+    Transaction[] incoming;
+
+    // outgoing funding
+    Transaction[] outgoing;
+
+    // contract information struct
+    struct ContractInfo {
+        string id;
+        address owner;
+        Transaction[] incoming;
+        Transaction[] outgoing;
+    }
+
     constructor(string memory _id) {
         owner = payable(msg.sender);
         id = _id;
     }
 
-    function retrieveInfo() public pure returns (string memory) {
-        // not returning transaction information yet
-        return "Hello World";
+    function retrieveInfo() public view returns (ContractInfo memory) {
+        return ContractInfo(id, owner, incoming, outgoing);
     }
 
     function donate() public payable {
         require(msg.value > 0, "You must send some Ether");
+        incoming.push(Transaction(msg.sender, msg.value));
     }
 
-    function withdraw() public payable {
-        require(authenticateAdmin(), "You are not the owner of this contract");
+    function withdraw() public payable onlyAdmin {
+        require(address(this).balance > 0, "There is no Ether to withdraw");
+        outgoing.push(Transaction(msg.sender, address(this).balance));
         owner.transfer(address(this).balance);
     }
 
-    function authenticateAdmin() public view returns (bool) {
-        require(owner == msg.sender, "You are not the owner of this contract");
+    function authenticateAdmin() public view onlyAdmin returns (bool) {
         return true;
+    }
+
+    modifier onlyAdmin() {
+        require(owner == msg.sender, "You are not the owner of this contract");
+        _;
     }
 }
