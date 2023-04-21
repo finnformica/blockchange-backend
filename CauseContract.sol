43ad6c0854da6ecf7cbcf8c9a349f60b2651e38f
finnformica <finnformica@gmail.com>
2023-04-21 09:51:32 +0100

    add donation fee

--- CauseContract.sol
+++ CauseContract.sol
@@ -7,13 +7,21 @@ contract CauseContract {
     // admin address
     address payable admin;
 
+    // charityx wallet address
+    address payable charityx;
+    uint256 feePercent = 1;
+
     // human-readable contract id
     string id;
 
     // transaction struct
     struct Transaction {
         address sender;
-        uint amount;
+        uint256 amount;
+        uint256 timestamp;
+        uint256 blockNumber;
+        uint256 gasUsed;
+        uint256 transactionFee;
     }
 
     // incoming donations
@@ -41,13 +49,19 @@ contract CauseContract {
 
     function donate() public payable {
         require(msg.value > 0, "You must send some Ether");
-        incoming.push(Transaction(msg.sender, msg.value));
+
+        incoming.push(Transaction(msg.sender, msg.value * (100 - feePercent) / 100, block.timestamp, block.number, tx.gasprice, 2));
+
+        charityx.transfer(msg.value * feePercent / 100);
     }
 
-    function withdraw() public payable onlyAdmin {
-        require(address(this).balance > 0, "There is no Ether to withdraw");
-        outgoing.push(Transaction(msg.sender, address(this).balance));
-        admin.transfer(address(this).balance);
+    function withdraw(uint256 _amount) public payable onlyAdmin {
+        require(address(this).balance > _amount, "There is no Ether to withdraw");
+        outgoing.push(Transaction(msg.sender, _amount, block.timestamp, block.number, tx.gasprice, 2));
+
+        // use the transfer method to transfer the amount to the admin's address
+        (bool success, ) = admin.call{value: _amount}("");
+        require(success, "Withdrawal failed");
     }
 
     function authenticateAdmin() public view onlyAdmin returns (bool) {
