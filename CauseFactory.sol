c09552f02fc0d9992865049085690a75e01785c8
finnformica <finnformica@gmail.com>
2023-05-04 18:14:44 +0100

    store all cause ids for easy retrieval

--- CauseFactory.sol
+++ CauseFactory.sol
@@ -5,6 +5,9 @@ import "./CauseContract.sol";
 
 contract CauseFactory {
 
+    // store all ids
+    string[] public ids;
+
     // store all deployed causes
     mapping(string => CauseContract) public deployedCauses;
 
@@ -16,10 +19,15 @@ contract CauseFactory {
         require(checkIfIdUnique(_id), "ID already exists");
         CauseContract newCause = new CauseContract(_id);
         deployedCauses[_id] = newCause;
+        ids.push(_id);
     }
 
     function cfRetrieveInfo(string memory _id) public view returns (CauseContract.ContractInfo memory) {
         return deployedCauses[_id].retrieveInfo();
+    }
+
+    function cfRetrieveIds() public view returns (string[] memory) {
+        return ids;
     }
 }
 
