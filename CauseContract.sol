1b45ff550e466da173649adf60a1cb4eba43ce66
Jordan Collingwood <jordycollingwood@gmail.com>
2023-05-22 14:01:28 +0100

    redist_push_info

--- CauseContract.sol
+++ CauseContract.sol
@@ -6,17 +6,29 @@ contract CauseContract {
     // admin address
     address payable admin;
 
-    // contract address
-    address payable contractAddress;
+    // Contract owner address
+    // address public owner;
+
+    // Contract address
+    address contractAddress = address(this);
 
     // blockChange wallet address
-    address payable blockChange;
+    address payable blockChange = payable(0xB860dDDA6F1506607bf42E2c9E871488216aE04D);
 
     // donation total tracker
     uint256 causeTotal;
 
+    // cause withdrawal total tracker
+    uint256 causeWithdrawalTotal;
+
+    // Flag for whether funds have been redistributed -> 1= False, 2=True
+    uint256 fundsDistributedFlag = 1;
+
+    // Transaction fee of 50bps (by default)
     uint256 constant BASIS_POINTS = 50;
-    
+    // Add transactionFeeBasisPoints variable for gas optimization
+    uint256 transactionFeeBasisPoints;
+
     // cause inputs
     string id;
     string name;
@@ -24,7 +36,7 @@ contract CauseContract {
     string websiteURL;
     string thumbnailURL;
     string email;
-    
+   
     // incoming donations
     Transaction[] incoming;
 
@@ -32,8 +44,9 @@ contract CauseContract {
     Transaction[] outgoing;
 
     // donor proportion tracking
-    mapping(address => uint256) public donorTotals;
-    
+    mapping(address => uint256) public donorTotals;    
+
+    // mapping used for unique address identification in redistribute funds function
     mapping(address => bool) public addressDonated;
 
     // causeState flag -> 1 = active, 2 = inactive
@@ -48,6 +61,7 @@ contract CauseContract {
         Transaction[] outgoing;
         address contractAddress;
         uint256 causeTotal;
+        uint256 causeWithdrawalTotal;
         uint256 causeState;
         string email;
         string description;
@@ -61,16 +75,14 @@ contract CauseContract {
         uint256 amount;
         uint256 timestamp;
         uint256 blockNumber;
-        uint256 gasUsed;
-        uint256 transactionFee;
     }
 
     constructor(string memory _id, string memory _name, address payable _admin, string memory _description, string memory _websiteURL, string memory _thumbnailURL, string memory _email) {
         admin = _admin;
-        contractAddress = payable(address(this));
 
-        blockChange = payable(msg.sender);
-        
+        // Calculate transactionFeeBasisPoints only once during contract creation
+        transactionFeeBasisPoints = BASIS_POINTS / 1000;
+       
         // initialise cause inputs
         id = _id;
         name = _name;
@@ -87,8 +99,9 @@ contract CauseContract {
             admin, 
             incoming, 
             outgoing, 
-            contractAddress, 
+            address(this),
             causeTotal, 
+            causeWithdrawalTotal,
             causeState, 
             email, 
             description, websiteURL, thumbnailURL);
@@ -98,42 +111,36 @@ contract CauseContract {
         require(msg.value > 0, "You must send some Ether");
         require(causeState == 1, "This cause has ended, your funds have been returned");
 
-        uint256 gasStart = gasleft();
+        // Use transactionFeeBasisPoints to calculate transactionFee
+        uint256 transactionFee = msg.value * transactionFeeBasisPoints;
 
-        uint256 transactionFee = (msg.value*BASIS_POINTS) / 1000; // Transaction fee of 5bps (by default)
-        
-        // (bool success, ) = blockChange.call{value: transactionFee}("");
-        // require(success, "Transfer failed.");
 
-        uint256 gasUsed = gasStart - gasleft();
-        uint256 gasPrice = tx.gasprice;
-        uint256 gasFee = gasUsed * gasPrice;
+        // `msg.value - transactionFee` was used twice -> store it in a variable to save gas
+        // Calculate netDonation and use it later for gas optimization
+        uint256 netDonation = msg.value - transactionFee;
 
         // update donor proportion
         donorTotals[msg.sender] += msg.value;
 
         // update causeTotal
-        causeTotal += (msg.value - transactionFee);
+        causeTotal += netDonation;
 
-        incoming.push(Transaction(msg.sender, msg.value - transactionFee, block.timestamp, block.number, gasFee, transactionFee));          
+        incoming.push(Transaction(msg.sender, netDonation, block.timestamp, block.number)); 
+
+        // Transfer the transactionFee as last stage of function execution
+        (bool success, ) = blockChange.call{value: transactionFee}("");
+        require(success, "Transfer failed.");         
     }
 
     function withdraw(uint256 _amount) public payable onlyAdmin {
-        require(address(this).balance > _amount, "Insufficient funds for withdrawal");
-        
-        uint256 gasStart = gasleft();
-        
-        causeTotal -= _amount;
+        require(address(this).balance >= _amount, "Insufficient funds for withdrawal");
+       
+        causeWithdrawalTotal += _amount;
 
-        // use the transfer method to transfer the amount to the admin's address
+        outgoing.push(Transaction(msg.sender, _amount, block.timestamp, block.number));
+
         (bool success, ) = admin.call{value: _amount}("");
         require(success, "Withdrawal failed");
-
-        uint256 gasUsed = gasStart - gasleft();
-        uint256 gasPrice = tx.gasprice;
-        uint256 gasFee = gasUsed * gasPrice;
-
-        outgoing.push(Transaction(msg.sender, _amount, block.timestamp, block.number, gasFee, 0));
     }
 
     function authenticateAdmin() public view onlyAdmin returns (bool) {
@@ -142,9 +149,16 @@ contract CauseContract {
 
     function updateAdmin(address _newAdmin) public onlyAdmin {
         admin = payable(_newAdmin);
+
+    }
+
+    function getAdmin() public view returns (address){
+        return admin;
+
     }
 
     function toggleCauseState() public onlyAdmin {
+        require(fundsDistributedFlag == 1, "Funds have been redistributed, please delete the cause");
         if (causeState == 1) {
             causeState = 2;
         }
@@ -155,12 +169,14 @@ contract CauseContract {
 
     function distributeFunds() public onlyAdmin {
         require(causeState == 2, "The cause has not ended yet");
+        require(fundsDistributedFlag == 1, "Funds have already been redistributed, please delete the cause");
         require(address(this).balance > 0, "The contract balance is zero");
+
 
         uint256 totalDonation = address(this).balance;
 
         // keep track of whether an address has already donated or not
-        for (uint256 i = 0; i < incoming.length; i++) {
+                for (uint256 i = 0; i < incoming.length; i++) {
             address sender = incoming[i].sender;
 
             // check if the address has already donated
@@ -168,6 +184,7 @@ contract CauseContract {
                 uint256 proportion = donorTotals[sender] * 100 / totalDonation;
                 uint256 donation = totalDonation * proportion / 100;
                 if (donation > 0) {
+                    outgoing.push(Transaction(sender, donation, block.timestamp, block.number));
                     (bool success, ) = sender.call{value: donation}("");
                     require(success, "Failed to distribute funds to donor");
                 }
@@ -176,15 +193,23 @@ contract CauseContract {
                 addressDonated[sender] = true;
             }
         }
+
+        fundsDistributedFlag = 2;
+
     }
 
     receive() external payable {
         donate();
     }
 
-    // modifier to ensure only admin is able to call function
+    function setCauseStateInactive() public onlyAdmin {
+        causeState = 2;
+    }
+
+    // modifier to ensure only admin or contract factory is able to call function
     modifier onlyAdmin() {
-        require(admin == msg.sender, "You are not the admin of this contract");
+        require(admin == msg.sender || blockChange == msg.sender, "You are not the admin of this contract");
         _;
     }
+
 }
